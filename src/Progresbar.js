9fe344ccc452844dd408d154c5575be5279d4f20
boma25 <amakirisamuelboma@gmail.com>
2020-08-15 00:53:52 +0100

    screensleft

--- src/Progresbar.js
+++ src/Progresbar.js
@@ -22,23 +22,66 @@ import {
 
 class Progresbar extends Component{
     state = {
-        message:first
+        message:first,
+        animation1 : new Animated.Value(0),
+        animation2 : new Animated.Value(0),
+        animation3 : new Animated.Value(0),
+        animation4 : new Animated.Value(0),
+        animation5 : new Animated.Value(0)
     }
 
+    componentDidMount(){
+        this.slidein()
+    }
+
+    slidein = ()=>{
+        Animated.sequence([
+            Animated.timing(this.state.animation1,{
+                toValue:66,
+                duration:5000,
+                useNativeDriver: false,
+            }),
+            Animated.timing(this.state.animation2,{
+                toValue:66,
+                duration:5000,
+                useNativeDriver: false,
+            }),
+            Animated.timing(this.state.animation3,{
+                toValue:66,
+                duration:5000,
+                useNativeDriver: false
+            }),
+            Animated.timing(this.state.animation4,{
+                toValue:66,
+                duration:5000,
+                useNativeDriver: false
+            }),
+            Animated.timing(this.state.animation5,{
+                toValue:66,
+                duration:5000,
+                useNativeDriver: false
+            }),
+        ]).start()
+    }
     render(){
-        
+            
         return(
             <View style={{alignItems: 'center', justifyContent: "space-around", height:height}}>
                 <View style={{flexDirection:"row", marginTop:-70, justifyContent: "center", }}>
                     <TouchableOpacity style={styles.progres}  >
+                        <Animated.View style={{height:13, width:this.state.animation1, backgroundColor:"white"}}/>
                     </TouchableOpacity> 
                     <TouchableOpacity style={styles.progres} onPress={()=> this.setState({message:second})}>
+                        <Animated.View style={{height:13, width:this.state.animation2, backgroundColor:"white"}}/>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.progres} >
+                        <Animated.View style={{height:13, width:this.state.animation3, backgroundColor:"white"}}/>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.progres}>
+                        <Animated.View style={{height:13, width:this.state.animation4, backgroundColor:"white"}}/>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.progres} >
+                        <Animated.View style={{height:13, width:this.state.animation5, backgroundColor:"white"}}/>
                     </TouchableOpacity>
                 </View>
                 <View style={{justifyContent: "center", alignItems:"center", paddingTop:70}}>
@@ -67,7 +110,7 @@ const styles = StyleSheet.create({
     progres :{
         width:width/6,
         height:15,
-        borderWidth:1,
+        borderWidth:2,
         borderColor: "#999",
         marginRight:10,
         borderRadius:5,
